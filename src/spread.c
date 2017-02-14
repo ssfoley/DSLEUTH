0354033cb055eda8d9ded13d893e51bf8618db0e
yuanzhihao <yuan.zhihao@flux.cs.uwlax.edu>
2017-02-13 21:21:08 -0600

    test

--- src/spread.c
+++ src/spread.c
@@ -163,7 +163,7 @@ static void
   for (i = 0; i < mem_GetTotalPixels(); ++i)
     {
       //printf("%f\n", z[i]);
-      fprintf(file, "%d\n", delta[i]);
+      fprintf(file, "%d\n", slp[i]);
     }
   printf("%s\n", "结束");
   char func[] = "spr_phase1n3";
