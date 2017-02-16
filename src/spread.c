0d5ee0f01f27f81fb9536341721453c648ed6e71
yuanzhihao <yuan.zhihao@flux.cs.uwlax.edu>
2017-02-16 16:41:54 -0600

    test

--- src/spread.c
+++ src/spread.c
@@ -1351,13 +1351,13 @@ void
                 sng,                                         /* IN/OUT */
                 sdc);                                      /* IN/OUT */
   printf("\n%d %d\n", *sng, *sdc);
-  FILE *file = fopen("debug.txt", "w");
+  //FILE *file = fopen("debug.txt", "w");
 
-  for (i = 0; i < mem_GetTotalPixels(); ++i)
-  {
-    fprintf(file, "%d\n", delta[i]);
-  }
-  printf("%s\n", "完成");
+  //for (i = 0; i < mem_GetTotalPixels(); ++i)
+  //{
+  //  fprintf(file, "%d\n", delta[i]);
+  //}
+  //printf("%s\n", "完成");
   timer_Stop (SPR_PHASE1N3);
 
   /*
@@ -1373,6 +1373,14 @@ void
               slp,                                           /* IN     */
               swght,                                         /* IN     */
               og);                                         /* IN/OUT */
+  printf("\n%d\n", *og);
+  FILE *file = fopen("debug.txt", "w");
+
+  for (i = 0; i < mem_GetTotalPixels(); ++i)
+    {
+      fprintf(file, "%d\n", delta[i]);
+    }
+  printf("%s\n", "完成");
   timer_Stop (SPR_PHASE4);
 
   /*
