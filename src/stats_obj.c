21c6591b8488de710d4e2c6afd728faa3647c060
yuan.zhihao <yuan.zhihao@uwlax.edu>
2017-02-16 23:51:15 -0600

    test

--- src/stats_obj.c
+++ src/stats_obj.c
@@ -913,7 +913,10 @@ static void
 {
   record[thread_id].this_year.growth_rate =
     record[thread_id].this_year.num_growth_pix / record[thread_id].this_year.pop * 100.0;
-  printf("\n%s %d %f %d\n", "set growth_rate", record[thread_id].this_year.num_growth_pix, record[thread_id].this_year.pop, thread_id);
+  #pragma omp critical
+  {
+    printf("\n%s %d %f %d\n", "set growth_rate", record[thread_id].this_year.num_growth_pix, record[thread_id].this_year.pop, thread_id);
+  }
 }
 /******************************************************************************
 *******************************************************************************
