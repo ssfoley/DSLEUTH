1e7e0a62db1ead04864b9a2dece32b39ca3d4e3c
yuan.zhihao <yuan.zhihao@uwlax.edu>
2017-02-16 21:43:11 -0600

    test

--- src/stats_obj.c
+++ src/stats_obj.c
@@ -1441,6 +1441,7 @@ static void
   double rad;
   double mean_cluster_size;
   int thread_id = omp_get_thread_num();
+  int i;
 
   total_pixels = mem_GetTotalPixels ();
   assert (total_pixels > 0);
@@ -1466,8 +1467,16 @@ static void
                        stats_workspace2);                  /* MOD    */
   #pragma omp critical
   {
-    printf("\n%d\n", z_ptr);
-    // printf("\n%s %f %f %f %f %f %f %f %f %d %s\n", "开始", area, edges, clusters, pop, xmean, ymean, slope, rad, mean_cluster_size, omp_get_thread_num(), "结束");
+    // printf("\n%d\n", z_ptr);
+    printf("\n%s %f %f %f %f %f %f %f %f %d %s\n", "开始", area, edges, clusters, pop, xmean, ymean, slope, rad, mean_cluster_size, omp_get_thread_num(), "结束");
+
+    FILE *file = fopen("debug.txt", "w");
+    fprintf(file, "%d\n", proc_GetCurrentYear());
+    for (i = 0; i < mem_GetTotalPixels(); ++i)
+    {
+      fprintf(file, "%d\n", z_ptr[i]);
+    }
+    printf("%s\n", "完成");
   }
   record[thread_id].this_year.area = area;
   record[thread_id].this_year.edges = edges;
