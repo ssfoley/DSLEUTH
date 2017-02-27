d3344515158c1b500ddb4e81849a6b8b54b2b1a8
yuan.zhihao <yuan.zhihao@uwlax.edu>
2017-02-27 17:01:54 -0600

    test

--- src/growth.c
+++ src/growth.c
@@ -244,7 +244,7 @@ void
     stats_SetRT (rt);
     stats_SetPOP (pop);
 
-    printf("%s\n", "end stats_set");
+    //printf("%s\n", "end stats_set");
 
     if (scen_GetViewGrowthTypesFlag ())
     {
@@ -260,7 +260,7 @@ void
                      255);
     }
 
-    //printf("%s\n", "end loop");
+    printf("%s\n", "end loop");
 
     if (scen_GetDoingLanduseFlag ())
     {
