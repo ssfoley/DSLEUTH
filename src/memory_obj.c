e0946af45fd74a806baf782c482722b7e3e870cd
yuan.zhihao <yuan.zhihao@uwlax.edu>
2017-03-15 17:48:02 -0500

    test

--- src/memory_obj.c
+++ src/memory_obj.c
@@ -636,10 +636,10 @@ static void
     {
       mem_check_array[mem_check_count++] = temp_ptr;
       temp_ptr += mem_check_size;
+      printf("%d %d\n", i, j);
       wgrid_array[i][j].ptr = (GRID_P) temp_ptr;
       mem_InvalidateGrid (wgrid_array[i][j].ptr);
       temp_ptr += wgrid_size;
-      printf("%d %d\n", i, j);
       strcpy (wgrid_array[i][j].current_owner, "");
       mem_wgrid_push (i, j);
       if (fp)
